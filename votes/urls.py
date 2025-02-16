359e39a70311f9f1ee0f09c3f1dc71994c7ff57c
DanielOpara <oparadaniv@gmail.com>
2025-02-16 20:01:28 +0100

    added get votes url

--- votes/urls.py
+++ votes/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from .views import VoterView
 
 urlpatterns = [
-    path("vote", VoterView.create_vote)
+    path("vote", VoterView.create_vote),
+    path('votes', VoterView.get_all)
 ]
